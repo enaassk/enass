6c2558a0dd826e1e22aed4e617d929d0b468cc82
enaassk <114976417+enaassk@users.noreply.github.com>
2022-11-01 16:58:29 +0600

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    char a;
+    cin >> a;
+    for (int i=0; i<a; i++) {
+        if (a >=48 && a<=57) {
+            cout << "yes";
+        }
+        else cout << "no";
+        break;
+    }
+}
